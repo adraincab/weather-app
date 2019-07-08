1fa34e3185f0c27e1c9a094f2a3ead885b0b3c82
unknown <adriancab@gmail.com>
2019-07-07 23:12:38 -0700

    added search functionality and styling

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,22 +6,36 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-fetch('http://localhost:3000/weather?address=boston').then((response) =>{
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
 
-        }
-        
-    })
-})
 
 
 const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit', () => {
-    console.log('testing!')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    // prevent default event that refreshes page
+    e.preventDefault()
+
+    const location = search.value
+    
+    messageOne.textConetent = 'Loading...'
+    messageTwo.textContent = ''
+
+        fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
+        response.json().then((data) => {
+            if(data.error){
+                messageOne.textContent = data.error
+            }else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+       
+
+            }
+            
+        })
+    })
 })
