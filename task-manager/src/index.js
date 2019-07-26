62eebcc6aec5d1cd299af2316999cf9594e97f3a
unknown <adriancab@gmail.com>
2019-07-26 00:18:18 -0700

    fetch all tasks and by task id

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,7 +14,7 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         res.status(201).send(user)
     }).catch((e) => {
-        res.status(400).send(e)
+        res.status(500).send(e)
     })
 })
 
@@ -47,9 +47,31 @@ app.post('/tasks', (req, res) => {
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
-        res.status(400).send(e)
+        res.status(500).send(e)
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(404).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+    
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(404).send()
+    })
+})
+
 
 
 app.listen(port, () => {
